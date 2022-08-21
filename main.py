72aca12474f9fce0e4dcb5ac7b3be650dfbb685b
DoctorStrange17 <111442798+DoctorStrange17@users.noreply.github.com>
2022-08-21 21:25:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ from tkinter import messagebox
 import ast
 import utils
 
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 window = Tk()
-window.title("Library mng")
-window.geometry("1200x650+350+200")
-window.configure(bg="#fff")
-window.resizable(False,False)
+window.title("Library Management")
+window.geometry("1280x800")
+window.resizable(True,True)
 #-------------------------------------
 
-heading=Label(window,text='Welcome', fg='#57a1f8',bg='white',font=('Cascadia Mono SemiBold',26,'bold'))
-heading.place(x=100,y=0)
 
+fl = Frame(window,width='300',height='800',bg='black').place(x=0,y=0)
+fr = Frame(window,width='980',height='800',bg='#eeeeee').place(x=300,y=0)
+#---------------------------------
 
-window.mainloop()
+def on_enter(e):
+   search_bar.delete(0,'end')
+def on_leave(e):
+   if search_bar.get()=='':
+      search_bar.insert(0,'Search')
+
+search_bar = Entry(fr,width=50,fg='black',border=0,bg='#eeeeee',
+            font=('Cascadia Mono Light',16))
+search_bar.place(x=350,y=20)
+search_bar.insert(0,'Search')
+search_bar.bind('<FocusIn>',on_enter)
+search_bar.bind('<FocusOut>',on_leave)
+
+Frame(fr,width=600,height=2,bg='black').place(x=350,y=50)
 
