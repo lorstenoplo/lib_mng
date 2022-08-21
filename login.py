edbd9c02f453c4a46993c4ba4ff854c114eb89d6
DoctorStrange17 <111442798+DoctorStrange17@users.noreply.github.com>
2022-08-22 00:11:42 +0530

    Update login.py

--- login.py
+++ login.py
@@ -4,12 +4,10 @@ from tkinter import messagebox
 import ast
 import utils
 
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
-
 window = Tk()
 window.title("Login")
-window.geometry("1200x650+350+200")
-window.configure(bg="#fff")
+window.geometry("1200x650")
+window.configure(bg="#fff") 
 window.resizable(False,False)
 #-----------------------------------
 def login():
@@ -23,20 +21,20 @@ def login():
       window.destroy()
       import main
    else:
-      messagebox.showerror('Sig In','Invalid credentials')
+      messagebox.showerror('Sign In','Invalid credentials')
 
 def show_signup():
    window.destroy()
    import signup
 
-frame=Frame(window,width=500,height=500,bg='white')
+frame=Frame(window,width=500,height=400,bg='white')
 frame.place(x=580,y=100)
 
 img=PhotoImage(file='login.png')
 Label(window,image=img,border=0,bg='white').place(x=120,y=140)
 
 heading=Label(frame,text='Sign In', fg='#57a1f8',bg='white',font=('Cascadia Mono SemiBold',26,'bold'))
-heading.place(x=100,y=0)
+heading.place(x=180,y=20)
 
 #---------------------------------
 def on_enter(e):
@@ -70,11 +68,12 @@ pwd.bind('<FocusOut>',on_leave)
 Frame(frame,width=430,height=2,bg='black').place(x=25,y=220)
 #---------------------------------
 
-Button(frame,width=33,pady=7,text='Sign In',bg='#57a1f8',fg='white',border=0,command=login,font=('Cascadia Mono SemiBold',11)).place(x=22,y=275)
+Button(frame,width=30,pady=7,text='Sign In',bg='#57a1f8',fg='white',
+       border=0,command=login,font=('Cascadia Mono SemiBold',11)).place(x=22,y=275)
 label=Label(frame,text='Dont have an account?',fg='black',bg='white',font=('Cascadia Mono Light',10))
 label.place(x=60,y=345)
 
 signup=Button(frame,width=7,text='Sign Up',border=0,bg='white',cursor='hand2',fg='#57a1f8',font=('Cascadia Mono Light',9),command=show_signup)
-signup.place(x=325,y=343)
+signup.place(x=380,y=345)
 
 window.mainloop()
