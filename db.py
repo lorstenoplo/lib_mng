ba8298bdedb942f9342c251f9ddd40ac2471a654
Nishanth <69671750+lorstenoplo@users.noreply.github.com>
2023-01-07 18:02:07 +0530

    Add files via upload

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ class Database:
         self.con = sqlite3.connect(db)
         self.cur = self.con.cursor()
         sql = """
-        CREATE TABLE IF NOT EXISTS employees(
+        CREATE TABLE IF NOT EXISTS books(
             id Integer Primary Key,
             name text,
             age text,
@@ -21,25 +21,25 @@ class Database:
 
     # Insert Function
     def insert(self, name, age, doj, email, gender, contact, address):
-        self.cur.execute("insert into employees values (NULL,?,?,?,?,?,?,?)",
+        self.cur.execute("insert into books values (NULL,?,?,?,?,?,?,?)",
                          (name, age, doj, email, gender, contact, address))
         self.con.commit()
 
     # Fetch All Data from DB
     def fetch(self):
-        self.cur.execute("SELECT * from employees")
+        self.cur.execute("SELECT * from books")
         rows = self.cur.fetchall()
         # print(rows)
         return rows
 
     # Delete a Record in DB
     def remove(self, id):
-        self.cur.execute("delete from employees where id=?", (id,))
+        self.cur.execute("delete from books where id=?", (id,))
         self.con.commit()
 
     # Update a Record in DB
     def update(self, id, name, age, doj, email, gender, contact, address):
         self.cur.execute(
-            "update employees set name=?, age=?, doj=?, email=?, gender=?, contact=?, address=? where id=?",
+            "update books set name=?, age=?, doj=?, email=?, gender=?, contact=?, address=? where id=?",
             (name, age, doj, email, gender, contact, address, id))
         self.con.commit()
